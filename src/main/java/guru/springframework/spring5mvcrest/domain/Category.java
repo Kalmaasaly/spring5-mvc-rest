5c160723ae5f075b53067048c32a9068161f6c92
Khalil <kalmaasaly@gmail.com>
2020-08-02 16:07:43 +0300

    a new project

---
+++ src/main/java/guru/springframework/spring5mvcrest/domain/Category.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring5mvcrest.domain;
+
+import lombok.Data;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+}
---
+++ src/main/java/guru/springframework/spring5mvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring5mvcrest.repositories;
+
+import guru.springframework.spring5mvcrest.domain.Category;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
